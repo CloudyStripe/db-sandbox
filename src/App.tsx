fbb91b7ae306595dacec4671f4cab2739b5cec52
Skyler Savard <ssavard@korterra.com>
2024-03-18 23:43:33 -0500

    form complete.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
+import { useState } from 'react';
 import './App.css'
 
-function App() {
+export const App: React.FC = () => {
+  const [userData, setUserData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault;
+    console.log('User submitted:', userData)
+  }
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  )
+    <form onSubmit={handleSubmit} className="signup-form">
+      <div className="form-fields">
+        <div className="form-group">
+          <label htmlFor="username" className="form-label">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="form-input"
+            value={userData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-input"
+            value={userData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-input"
+            value={userData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className="submit-button">Sign Up</button>
+      </div>
+    </form>
+  );
 }
 
 export default App
